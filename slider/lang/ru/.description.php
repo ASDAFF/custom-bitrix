3203dd75871944dc8525ecca406b2b009e9bc1e6
ASDAFF <asdaff.asad@yandex.ru>
2017-03-24 20:45:07 +0500

    message

---
+++ slider/lang/ru/.description.php
@@ -0,0 +1,6 @@
+<?
+$MESS ['T_IBLOCK_DESC_LIST'] = "Список новостей";
+$MESS ['T_IBLOCK_DESC_LIST_DESC'] = "Список новостей из одного информационного блока.";
+$MESS ['T_IBLOCK_DESC_NEWS'] = "Статьи и новости";
+$MESS ['IBLOCK_BLOCK_NAME_NBRAINS'] = "Слайдер";
+?>
